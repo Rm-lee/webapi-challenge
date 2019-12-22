29acd7337d5a95817d2bc90d95646c72ea4feb5f
rogerLee <rmlee0003@gmail.com>
2019-12-22 18:43:22 -0500

    created get endpoint

--- data/helpers/projectsRouter.js
+++ data/helpers/projectsRouter.js
@@ -1,8 +1,23 @@
 const express = require('express')
 
-const projs = require('./projectModel.js')
+const Projs = require('./projectModel.js')
 const actions = require('./actionModel.js')
 
 const router = express.Router()
+
+router.get('/', (req,res) => {
+    console.log(req.query)
+
+    Projs.get(req.query.id)
+    .then(projs => {
+        res.status(200).json(projs)
+    })
+    .catch(err => {
+        console.log(err)
+        res.status(500).json({
+            message: "There was an error retrieving projects"
+        })
+    })
+})
 
 module.exports = router
